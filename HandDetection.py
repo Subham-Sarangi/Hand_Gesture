e61a94751d749cadafe72b726998aa176ad1220c
Subham-Sarangi <56386795+Subham-Sarangi@users.noreply.github.com>
2021-12-15 19:48:12 +0530

    Create HandDetection.py

---
+++ HandDetection.py
@@ -0,0 +1,89 @@
+import mediapipe as mp
+import cv2
+import numpy as np
+import uuid
+import os
+
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+
+cap = cv2.VideoCapture(0)
+
+class HandDetector:
+    def __init__(self, max_num_hands=2, min_detection_confidence=0.5, min_tracking_confidence=0.5):
+        #when the mediapipe is first started, it detects the hands. After that it tries to track the hands
+        #as detecting is more time consuming than tracking. If the tracking confidence goes down than the
+        #specified value then again it switches back to detection
+        self.hands = mpHands.Hands(max_num_hands=max_num_hands, min_detection_confidence=min_detection_confidence,
+                                   min_tracking_confidence=min_tracking_confidence)
+
+
+    def findHandLandMarks(self, image, handNumber=0, draw=False):
+        originalImage = image
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # mediapipe needs RGB
+        results = self.hands.process(image)
+        landMarkList = []
+
+        if results.multi_handedness:
+            label = results.multi_handedness[handNumber].classification[0].label  # label gives if hand is left or right
+            #account for inversion in webcams
+            if label == "Left":
+                label = "Right"
+            elif label == "Right":
+                label = "Left"
+
+
+        if results.multi_hand_landmarks:  # returns None if hand is not found
+            hand = results.multi_hand_landmarks[handNumber] #results.multi_hand_landmarks returns landMarks for all the hands
+
+            for id, landMark in enumerate(hand.landmark):
+                # landMark holds x,y,z ratios of single landmark
+                imgH, imgW, imgC = originalImage.shape  # height, width, channel for image
+                xPos, yPos = int(landMark.x * imgW), int(landMark.y * imgH)
+                landMarkList.append([id, xPos, yPos, label])
+
+            if draw:
+                mpDraw.draw_landmarks(originalImage, hand, mpHands.HAND_CONNECTIONS)
+
+        return landMarkList
+with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) as hands: 
+    while cap.isOpened():
+        ret, frame = cap.read()
+        
+        # BGR 2 RGB
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        
+        # Flip on horizontal
+        image = cv2.flip(image, 1)
+        
+        # Set flag
+        image.flags.writeable = False
+        
+        # Detections
+        results = hands.process(image)
+        
+        # Set flag to true
+        image.flags.writeable = True
+        
+        # RGB 2 BGR
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        
+        # Detections
+        print(results)
+        
+        # Rendering results
+        if results.multi_hand_landmarks:
+            for num, hand in enumerate(results.multi_hand_landmarks):
+                mp_drawing.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS, 
+                                        mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
+                                        mp_drawing.DrawingSpec(color=(250, 44, 250), thickness=2, circle_radius=2),
+                                         )
+            
+        
+        cv2.imshow('Hand Tracking', image)
+
+        if cv2.waitKey(10) & 0xFF == ord('c'):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
