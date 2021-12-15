2e22b57a9148b4a2bf2a5b90241668551eb2a5fd
Subham-Sarangi <56386795+Subham-Sarangi@users.noreply.github.com>
2021-12-15 19:12:01 +0530

    Update Access_Webcam.py

--- Access_Webcam.py
+++ Access_Webcam.py
@@ -1,14 +1,14 @@
-import cv2
 #import opencv
+import cv2
 
-video = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)
 
 #Press the waitkey i.e. c to close the camera.
-while True:
-    ret, image = video.read()
+while cap.isOpened():
+    ret, image = cap.read()
     cv2.imshow("WebCam Access", image)
     k = cv2.waitKey(1)
     if k==ord('c'):
         break
-video.release()
+cap.release()
 cv2.destroyAllWindows()
